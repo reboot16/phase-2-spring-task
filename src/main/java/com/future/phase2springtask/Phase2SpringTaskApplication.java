f829274f4752c37cad96b384140788eaf338d947
alexandernsalim <twinzeno21@gmail.com>
2019-01-08 17:50:16 +0700

    Initial Commit

---
+++ src/main/java/com/future/phase2springtask/Phase2SpringTaskApplication.java
@@ -0,0 +1,14 @@
+package com.future.phase2springtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase2SpringTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase2SpringTaskApplication.class, args);
+	}
+
+}
+
